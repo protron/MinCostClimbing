ddf2b818aeedbce2f91a661975bd49a337a9ab67
Mariano Desanze <protronm@gmail.com>
2023-03-24 23:08:40 -0300

    2nd try: DynamicProgramming (cache) for new big case

--- Solution.cs
+++ Solution.cs
@@ -1,18 +1,34 @@
-public class Solution {
-    public int MinCostClimbingStairs(int[] cost) {
-        return Calc(cost, 0);
+public class Solution
+{
+    private int[] input;
+    private int?[] cache;
+
+    public int MinCostClimbingStairs(int[] cost)
+    {
+        this.input = cost;
+        this.cache = new int?[cost.Length];
+        return Calc(0);
     }
 
-    public int Calc(int[] cost, int firstIndex) {
-        var missing = cost.Length - firstIndex;
-        if (missing < 2) {
+    public int Calc(int firstIndex)
+    {
+        if (cache[firstIndex].HasValue)
+        {
+            return cache[firstIndex].Value;
+        }
+        var missing = input.Length - firstIndex;
+        if (missing < 2)
+        {
             return 0;
         }
-        if (missing == 2) {
-            return Math.Min(cost[firstIndex], cost[firstIndex + 1]);
+        if (missing == 2)
+        {
+            return Math.Min(input[firstIndex], input[firstIndex + 1]);
         }
-        var costSingleStep = cost[firstIndex] + Calc(cost, firstIndex + 1);
-        var costDoubleStep = cost[firstIndex + 1] + Calc(cost, firstIndex + 2);
-        return Math.Min(costSingleStep, costDoubleStep);
+        var costSingleStep = input[firstIndex] + Calc(firstIndex + 1);
+        var costDoubleStep = input[firstIndex + 1] + Calc(firstIndex + 2);
+        var result = Math.Min(costSingleStep, costDoubleStep);
+        cache[firstIndex] = result;
+        return result;
     }
 }
