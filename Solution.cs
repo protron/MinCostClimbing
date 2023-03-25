292de8206718fc21d49544bf5b6c3654bd77b3a4
Mariano Desanze <protronm@gmail.com>
2023-03-25 01:53:33 -0300

    Avoid recursion and cache. Time:O(n) Mem:O(1)

--- Solution.cs
+++ Solution.cs
@@ -2,41 +2,12 @@ public class Solution
 {
     public int MinCostClimbingStairs(int[] cost)
     {
-        var subClass = new SubClass(cost);
-        return subClass.Calc(0);
-    }
-
-    private class SubClass
-    {
-        public int[] input;
-        public int?[] cache;
-
-        public SubClass(int[] input)
+        int s1 = cost[0];
+        int s2 = cost[1];
+        for (int i = 2; i < cost.Length; i++)
         {
-            this.input = input;
-            this.cache = new int?[input.Length];
+            (s1, s2) = (s2, cost[i] + Math.Min(s1, s2));
         }
-
-        public int Calc(int firstIndex)
-        {
-            if (cache[firstIndex] is int cacheItem)
-            {
-                return cacheItem;
-            }
-            var missing = input.Length - firstIndex;
-            if (missing < 2)
-            {
-                return 0;
-            }
-            if (missing == 2)
-            {
-                return Math.Min(input[firstIndex], input[firstIndex + 1]);
-            }
-            var costSingleStep = input[firstIndex] + Calc(firstIndex + 1);
-            var costDoubleStep = input[firstIndex + 1] + Calc(firstIndex + 2);
-            var result = Math.Min(costSingleStep, costDoubleStep);
-            cache[firstIndex] = result;
-            return result;
-        }
+        return Math.Min(s1, s2);
     }
 }
