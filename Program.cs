41d554c4482bbcc4f6dcb73ad677b008acd0d537
Mariano Desanze <protronm@gmail.com>
2023-03-25 02:14:41 -0300

    Fix lineNumber

--- Program.cs
+++ Program.cs
@@ -5,19 +5,19 @@ var solution = new Solution();
 var summary = (successes: 0, failures: 0);
 var testCases = File.ReadAllLines("TestCases.jsonl");
 var expectedResults = File.ReadAllLines("ExpectedResults.txt");
-var items = Enumerable.Range(0, testCases.Length).Zip(testCases, expectedResults);
+var items = Enumerable.Range(1, testCases.Length).Zip(testCases, expectedResults);
 foreach (var item in items) {
-    var index = item.First;
+    var lineNumber = item.First;
     var testCase = item.Second;
     int expected = int.Parse(item.Third);
     int[]? input = JsonSerializer.Deserialize<int[]>(testCase);
     Debug.Assert(input is not null);
     int actual = solution.MinCostClimbingStairs(input);
     if (expected != actual) {
-        Write(ConsoleColor.Red, $"ERROR at line {index}: expected {expected} but actually got {actual}. TestCase: {testCase}");
+        Write(ConsoleColor.Red, $"ERROR at line {lineNumber}: expected {expected} but actually got {actual}. TestCase: {testCase}");
         summary.failures += 1;
     } else {
-        Write(ConsoleColor.Green, $"Line {index}: TestCase: {testCase} returned expected {expected}.");
+        Write(ConsoleColor.Green, $"Line {lineNumber}: TestCase: {testCase} returned expected {expected}.");
         summary.successes += 1;
     }
 }
